96e492a8b171fea485f827d7768e977321000bb2
nelya@ginger <struchkovanelya@gmail.com>
2017-06-25 17:36:18 +0300

    php basic tasks in new branch

---
+++ php_basics_tasks/8.php
@@ -0,0 +1,13 @@
+<?php
+
+include '2.php';
+
+if ( $age >= 18 && $age <= 59 ) {
+    echo "Вам еще работать и работать";
+} elseif ( $age > 59 ) {
+    echo "Вам пора напенсию";
+} elseif ( $age < 18 && $age > 0 ) {
+    echo "Вам еще рано работать";
+} elseif ( $age < 0 || $age != is_int($age) ) {
+    echo "Неизвестный возраст";
+};
